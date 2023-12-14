60e7c67b01b982eba314c742a1c86aae386dc07e
choidaae <128240797+choidaae@users.noreply.github.com>
2023-12-14 23:39:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('gameBoard');
-    const cardValues = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg']; // 책 표지 대신 임시로 사용
+    const countdownElement = document.getElementById('countdown');
+    const startButton = document.getElementById('startButton');
+    const overlay = document.getElementById('overlay');
+    const cardValues = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg'];
     let selectedCards = [];
-    let canFlip = true;
+    let canFlip = false;
+    let countdownTimer;
+    let matchedCount = 0;
+    const totalPairs = cardValues.length;
 
-    // 카드 섞기 및 보드 초기화
-    function initializeBoard() {
-        let mixedValues = shuffle([...cardValues, ...cardValues]);
-        mixedValues.forEach(value => {
-            gameBoard.appendChild(createCard(value));
-        });
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
     }
 
-    // 카드 생성
     function createCard(value) {
         let card = document.createElement('div');
         card.classList.add('card');
@@ -23,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
         let cardBack = document.createElement('div');
         cardBack.classList.add('card-face', 'card-back');
-        cardBack.style.backgroundImage = `url('./${value}')`; // 이미지 경로 설정
+        cardBack.style.backgroundImage = `url('./${value}')`;
     
         card.appendChild(cardFront);
         card.appendChild(cardBack);
@@ -31,7 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
         return card;
     }
 
-    // 카드 뒤집기
     function flipCard(card) {
         if (!canFlip || card.classList.contains('is-flipped')) return;
         card.classList.add('is-flipped');
@@ -42,39 +46,75 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // 매칭 확인
     function checkForMatch() {
-        canFlip = false;
         let [card1, card2] = selectedCards;
-
         if (card1.dataset.value === card2.dataset.value) {
+            matchedCount++;
             selectedCards = [];
-            canFlip = true;
+            if (matchedCount === totalPairs) {
+                clearInterval(countdownTimer);
+                Swal.fire({
+                    icon: 'success',
+                    title: '게임 성공!',
+                    toast: true,
+                    showConfirmButton: false
+                });
+            }
         } else {
             setTimeout(() => {
                 card1.classList.remove('is-flipped');
                 card2.classList.remove('is-flipped');
                 selectedCards = [];
-                canFlip = true;
-            }, 1000);
+            }, 800);
         }
+        canFlip = false;
+        setTimeout(() => {
+            canFlip = true;
+        }, 1000);
     }
 
-    // 배열 섞기 (Fisher-Yates Shuffle)
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
+    function startCountdown(duration) {
+        let timer = duration, minutes, seconds;
+        countdownTimer = setInterval(() => {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            countdownElement.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                clearInterval(countdownTimer);
+                canFlip = false;
+                Swal.fire({
+                    icon: 'error',
+                    title: '아쉬워요!',
+                    toast: true,
+                    showConfirmButton: false
+                });
+            }
+        }, 1000);
     }
 
-    // 게임 보드에 클릭 이벤트 리스너 추가
+    function initializeBoard() {
+        let mixedValues = shuffle([...cardValues, ...cardValues]);
+        mixedValues.forEach(value => {
+            gameBoard.appendChild(createCard(value));
+        });
+    }
+
     gameBoard.addEventListener('click', event => {
         let clickedCard = event.target.closest('.card');
         if (clickedCard) {
             flipCard(clickedCard);
         }
+    });
+
+    startButton.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        startCountdown(90); // 1분 30초 (90초) 카운트다운
+        canFlip = true;
     });
 
     initializeBoard();
